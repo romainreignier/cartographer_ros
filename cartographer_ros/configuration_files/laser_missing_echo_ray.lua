53718c7826eb60f1e3c8ec3c6fdfa057ab87e91b
rre <rre@robopec.com>
2017-05-30 16:56:07 +0200

    Add a filter to reduce gazebo misses fans.

--- cartographer_ros/configuration_files/laser_missing_echo_ray.lua
+++ cartographer_ros/configuration_files/laser_missing_echo_ray.lua
@@ -35,8 +35,8 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.05
-TRAJECTORY_BUILDER_2D.max_range = 10.
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 8.
+TRAJECTORY_BUILDER_2D.max_range = 9.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 6.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 
